401f73cc35a2dafc42be254faf9891a109ca7b9d
sluongo718 <sluongo718@yahoo.com>
2021-03-16 23:11:52 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,16 @@
 class TweetsController < ApplicationController
 
+    get "/tweets" do 
+        @tweets = Tweet.all
+        if logged_in?(session)
+            erb :"tweets/tweets"
+        else
+            redirect "/login"
+        end
+    end
+
+    post "/tweets" do 
+
+    end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,42 @@
 class UsersController < ApplicationController
 
+    get "/signup" do 
+        if !logged_in?(session)
+            erb :"/signup"
+        else
+            redirect '/tweets'
+
+        end
+    end
+
+    post "/signup" do 
+        if  params[:username].empty? || params[:email].empty? || params[:password].empty?
+            redirect "/signup"
+        else
+            @user = User.create(params)
+            session[:user_id] = @user.id
+            @user.save
+            redirect "/tweets"
+        end
+    end
+
+    get "/login" do 
+        if !logged_in?(session)
+            erb :"/login"
+        else
+            redirect "/tweets"
+        end
+    end
+
+    post "/login" do 
+        @user = User.find_by(username: params[:username])
+        if @user && user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect "/tweets"
+        else
+            redirect "/signup"
+        end
+    end
+
 
 end
